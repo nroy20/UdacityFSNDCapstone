408e53eaefe1936dba422fca3c104c8536a47726
nroy20 <nroy20@illinois.edu>
2020-08-04 13:34:13 -0500

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,3 @@
+export AUTH0_DOMAIN='my-casting-agency-capstone.us.auth0.com'
+export AUTH0_CLIENT_SECRET='4iE-1lpo4GXmx9JagQux-nfzJA3JfNWbDq1-eMVoFE2eOrojzLnwghjWTI26x49C'
+export AUTH0_CLIENT_ID='tBzEjpeO5w5XC6P2RtO3AGdW5N4ydlN1'
