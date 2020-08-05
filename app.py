11a49d97e9e7d873d497e107a7638b94e86b3034
nroy20 <nroy20@illinois.edu>
2020-08-05 00:35:11 -0500

    Updated login link

--- app.py
+++ app.py
@@ -315,4 +315,4 @@ app = create_app()
 app.secret_key = os.getenv('SECRET_KEY')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
