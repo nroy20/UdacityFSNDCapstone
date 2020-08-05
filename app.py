cffac67ae7424298ae8002d44b1bfbb6c9544b26
nroy20 <nroy20@illinois.edu>
2020-08-05 15:16:46 -0500

    a

--- app.py
+++ app.py
@@ -279,7 +279,10 @@ def create_app(test_config=None):
         token = body.get('token')
         if not token:
             return render_template('home_page.html', token="404"), 404
-        return render_template('home_page.html', token="yay"), 200
+        return jsonify({
+            "success": True,
+            "token": token
+        })
 
     @app.route('/')
     def index():
