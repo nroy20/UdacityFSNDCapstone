a094b300bbdde4d7a87ab733b9bddfda2a79c3dd
nroy20 <nroy20@illinois.edu>
2020-08-05 12:09:58 -0500

    a

--- app.py
+++ app.py
@@ -272,9 +272,9 @@ def create_app(test_config=None):
 
     @app.route('/login-results', methods=['GET'])
     def set_token():
-        body = request.get_json()
-        token = body.get('token')
-        return render_template('home_page.html', token), 200
+        #body = request.get_json()
+        #token = body.get('token')
+        return render_template('home_page.html'), 200
 
     @app.route('/')
     def index():
